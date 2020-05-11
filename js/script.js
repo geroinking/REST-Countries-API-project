42725961733515392e69eb07db7082c6fbcf2942
geroinking <russel95smart@gmail.com>
2020-05-11 17:12:20 +0300

    new rendered cards after sortion

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ let input = document.getElementById('input'),
   country = document.querySelectorAll('.country'),
   countries = document.querySelector('.countries'),
   section = document.getElementById('section'),
-  topForm = document.querySelector('.top_form');
-let filterCountry;
+  formWrapper = document.querySelector('.form_wrapper'),
+  headerWrapper = document.querySelector('.header-wrapper');
 
 const getData = async function (url) {
 
@@ -23,28 +23,35 @@ const getData = async function (url) {
 };
 
 function goToMain(event) {
-const newCard = document.getElementById('new_card');
-topForm.style.display = 'flex';
-newCard.innerHTML = '';
+  const newCard = document.getElementById('new_card');
+  formWrapper.style.display = 'block';
+  newCard.innerHTML = '';
 }
 
 //search by name
-
 function search(name) {
   countries.innerHTML = '';
   name = input.value;
   getData(`https://restcountries.eu/rest/v2/name/${name}`)
-  .then(function(data) {
-    topForm.style.display = 'none';
-    data.forEach(renderNewCard);
-  }).catch(function (err) {
-    console.log('Fetch Error :-S', err);
-  });
+    .then(function (data) {
+
+      formWrapper.style.display = 'none';
+      data.forEach(renderNewCard);
+
+    }).catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
 }
 
 //sort by region
-function renderCard({ name, flag, population, region, capital }) {
-  
+function renderCard({
+  name,
+  flag,
+  population,
+  region,
+  capital
+}) {
+
   const card = `
             <div id="${name}" class="country">
                 <img  src="${flag}" alt=""> <br>
@@ -54,10 +61,12 @@ function renderCard({ name, flag, population, region, capital }) {
                 <span>Capital: ${capital}</span>
             </div>`;
 
+
   countries.insertAdjacentHTML('afterBegin', card);
 }
 
 function renderNewCard(country) { //rendering new card after search; getting country object
+  //TODO get values from currencies, languages, borders arrays
   const {
     flag,
     name,
@@ -72,15 +81,9 @@ function renderNewCard(country) { //rendering new card after search; getting cou
     borders
   } = country;
 
-  currencies.forEach(function(e) {
-    let currencyName = e.name;
-    // console.log(currencyName);
-  });
-  console.log(currencies, languages, borders);
-  
   const card = `
    <div id="new_card">
-                <button id="button_back">Back</button>
+                <button id="button_back">Close</button>
                 <div class="country_wrapper">
                     <div class="country_flag">
                         <image class="country_flag_flag" alt="flag" src="${flag}">
@@ -106,74 +109,63 @@ function renderNewCard(country) { //rendering new card after search; getting cou
                     </div>
                 </div>
             </div>
-  `;
- 
+  `; //TODO: get values from the borders array, and set as links to another countries
+
   section.insertAdjacentHTML('afterbegin', card);
   const buttonBack = document.getElementById('button_back');
   buttonBack.addEventListener('click', goToMain);
-
 }
 
-function loadNewCard(name) {  //got onclick array with sorted country
-  getData(`https://restcountries.eu/rest/v2/name/${name}`)
-  .then(function(data) {
-    section.innerHTML = '';
-    renderNewCard(data);
-  });
+function renderCardOnclick(event) {
+  const target = event.target;
+  const name = target.closest('.country');
+  const countryName = name.id;
+  if (countryName) {
+    getData(`https://restcountries.eu/rest/v2/name/${countryName}`)
+      .then(function (data) {
+        data.forEach(function (item) {
+          renderNewCard(item); // passing object with proper name to render
+          countries.style.marginTop = '40px';
+        });
+      });
+  }
 }
 
 function loadCards() {
   countries.innerHTML = '';
   let region = event.target.value;
   getData(`https://restcountries.eu/rest/v2/region/${region}`)
-  .then(function (data) {
-    data.forEach(renderCard);
 
-    // country.forEach(addEventListener('click', function(event) {
+    .then(function (data) {
+      data.forEach(renderCard);
+    })
 
-    //   const target = event.target;
-    //   const name = target.closest('.country');
-
-    //   if (name.id) { //TODO name.id is null while click on empty space after sort
-    //     let countryName = name.id;
-    //     loadNewCard(countryName);
-    //   } else {
-    //     event.preventDefault();
-    //   }
-    // }));
-  })
-  .catch(function (err) {
-    console.log('Fetch Error :-S', err);
-  });
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
 }
 
 function changeToLightTheme() {
   buttonLight.style.display = 'none';
   buttonDark.style.display = 'block';
   body.style.color = 'hsl(209, 26%, 17%)';
-  body.style.backgroundColor = 'hsl(0, 0%, 100%)';
-  section.style.backgroundColor = 'hsl(0, 0%, 98%)';
-  country.forEach( function(e) {
-    e.style.backgroundColor = 'rgb(236, 235, 235)';
-  });
+  headerWrapper.style.backgroundColor = 'hsl(0, 0%, 100%)';
+  body.style.backgroundColor = 'hsl(0, 0%, 95%)';
 }
 
 function changeToDarkTheme() {
   buttonDark.style.display = 'none';
   buttonLight.style.display = 'block';
   body.style.color = 'hsl(0, 0%, 100%)';
-  body.style.backgroundColor = 'hsl(209, 23%, 22%)';
-  section.style.backgroundColor = 'hsl(207, 26%, 17%)';
-  country.forEach( function(e) {
-    e.style.backgroundColor = 'hsl(209, 23%, 22%)';
-  });
-
+  headerWrapper.style.backgroundColor = 'hsl(209, 23%, 22%)';
+  body.style.backgroundColor = 'hsl(207, 26%, 17%)';
 }
 
 
 
 function init() {
 
+  countries.addEventListener('click', renderCardOnclick);
 
   filter.addEventListener('change', loadCards);
 
